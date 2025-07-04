74169128aa9c42562aacac5db7a07269174f9da3
azis003 <azis003@gmail.com>
2025-07-04 15:06:04 +0700

    menggunakancontext

--- user/repository.go
+++ user/repository.go
@@ -21,7 +21,7 @@ func (r *repository) Save(ctx context.Context, user User) (User, error) {
 	query := `INSERT INTO users (name, occupation, email, password_hash, role, created_at, updated_at)
 				VALUES($1, $2, $3, $4, $5, NOW(), NOW())`
 
-	_, err := r.db.Exec(query, user.Name, user.Occupation, user.Email, user.PasswordHash, user.Role)
+	_, err := r.db.ExecContext(ctx, query, user.Name, user.Occupation, user.Email, user.PasswordHash, user.Role)
 	if err != nil {
 		return user, err
 	}
